e3a5222280b69cd95ffb75570f9cda9c409580c4
myunghun.chun <cmh2584@gmail.com>
2025-12-19 13:34:32 +0900

    Fix my blog

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// Small niceties (smooth scroll)
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click', e=>{
+    const target = document.querySelector(a.getAttribute('href'));
+    if(target){ e.preventDefault(); target.scrollIntoView({behavior:'smooth'}); }
+  });
+});
